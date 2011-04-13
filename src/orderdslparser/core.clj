c6c0b0d309b5e7f51cf3db1c6488cf718ff947b8
Steen Larsen <steen666@gmail.com>
2011-04-13 17:59:33 +0200

    first

---
+++ src/orderdslparser/core.clj
@@ -0,0 +1,29 @@
+(ns orderdslparser.core
+  (:use name.choi.joshua.fnparse))
+
+(def digit (lit-alt-seq  ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9"]))
+
+(def hex (alt digit "a" "b" "c" "d" "e" "f"))
+
+(def end (lit ";"))
+
+(def kundenummer (term #(re-find #"^[0-9]{9}$" %)))
+
+(def customer-id (conc (lit-conc-seq ["kunde" "nummer" ":"]) kundenummer end))
+
+(def payer-id (conc (lit-conc-seq ["betaler" "nummer" ":"]) kundenummer end))
+
+(def order (conc customer-id (opt payer-id)))
+
+(defn to-lower-case [token-string]
+  (.toLowerCase token-string))
+
+(def stop-words #{":" ";"})
+
+(defn tokenizer [string]
+  (map to-lower-case (re-seq #"\w+|\S|\d" string)))
+
+(defn parse-dsl [dsl]
+  (let [tokens (tokenizer dsl)]
+    (prn tokens)
+    (rule-match order #(println "FAILED: " %) #(println "LEFTOVER: " %2) {:remainder tokens})))
---
+++ test/orderdslparser/test/core.clj
@@ -0,0 +1,6 @@
+(ns orderdslparser.test.core
+  (:use [orderdslparser.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
